d117e7f53cd25bf30b190cf515720912d860c699
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-10-10 16:52:42 +0200

    Initial commit

---
+++ src/jdbc/ConnectionTest.java
@@ -0,0 +1,25 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionTest {
+
+	public static void main(String[] args) {
+
+		String url = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		
+		try {
+			System.out.println("Start connecting to database: " + url);
+			Connection connection =
+					DriverManager.getConnection(url, user, password);
+			System.out.println("\nConnection successful!");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
