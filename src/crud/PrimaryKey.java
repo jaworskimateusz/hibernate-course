b0ba19091e9e1bbf582845420e67d501c2a62a62
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-10-13 15:45:50 +0200

    Reading Objects

---
+++ src/crud/PrimaryKey.java
@@ -0,0 +1,45 @@
+package crud;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.Student;
+
+public class PrimaryKey {
+
+public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			realizeTransaction(session, createStudent("Mike", "Apple", "apple@email.com"));
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		
+	}
+	
+	private static Student createStudent(String firstName, String lastName, String email) {
+		return new Student(firstName, lastName, email);
+	}
+	
+	private static void realizeTransaction(Session session, Student car) {
+		session.beginTransaction();
+		session.save(car);
+		session.getTransaction().commit();
+		getTransactionResult();
+	}
+	
+	private static void getTransactionResult() {
+		System.out.println("\nTransaction successful");
+	}
+
+}
---
+++ src/crud/ReadStudent.java
@@ -0,0 +1,39 @@
+package crud;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import entity.Student;
+
+public class ReadStudent {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+			System.out.println("\nReading a student object.");
+			session.beginTransaction();
+			System.out.println(getNewStudent(session, new Student(), 1));
+			session.getTransaction().commit();
+			System.out.println("\nTransaction successful.");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			factory.close();
+		}
+	}
+	private static String getNewStudent(Session session, Student student, int id) {
+		student = session.get(Student.class, id);
+		return (student != null) ? student.toString()
+				: "Student not found";
+	}
+
+}
